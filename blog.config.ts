1f69a2804c2c73a7ebe51fc556065c33c29d8fdf
VacuolePaoo <161350352+VacuolePaoo@users.noreply.github.com>
2026-02-08 18:23:52 +0800

    conf: 完善配置

--- blog.config.ts
+++ blog.config.ts
@@ -1,26 +1,26 @@
 import type { FeedEntry } from './app/types/feed'
 
 const basicConfig = {
-	title: '博客',
-	subtitle: '博客',
+	title: '液泡部落格',
+	subtitle: '一介书生，三尺微命。',
 	// 长 description 利好于 SEO
-	description: '博客',
+	description: 'vacuole 的个人博客，分享技术探索与日常思考。专注于技术实践与知识沉淀，在学习、开源探索与生活感悟中持续记录。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
 	author: {
-		name: '博客',
-		avatar: 'https://www.example.site/api/avatar.png',
-		email: 'hi@example.cyou',
-		homepage: 'https://www.example.site/',
+		name: 'vacuole',
+		avatar: 'https://blog.vacu.top/assets/img/avatar.png',
+		email: 'i@vacu.top',
+		homepage: 'https://www.vacu.top/',
 	},
 	copyright: {
 		abbr: 'CC BY-NC-SA 4.0',
 		name: '署名-非商业性使用-相同方式共享 4.0 国际',
 		url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
 	},
-	favicon: 'https://www.example.site/api/icon.png',
+	favicon: '/favicon.ico',
 	language: 'zh-CN',
-	timeEstablished: '2019-07-19',
+	timeEstablished: '2024-07-19',
 	timeZone: 'Asia/Shanghai',
-	url: 'https://blog.example.site/',
+	url: 'https://blog.vacu.top/',
 	defaultCategory: '未分类',
 }
 
@@ -37,6 +37,8 @@ const blogConfig = {
 			杂谈: { icon: 'ph:chat-bold', color: '#3ba' },
 			生活: { icon: 'ph:shooting-star-bold', color: '#f77' },
 			代码: { icon: 'ph:code-bold', color: '#77f' },
+			学习: { icon: 'ph:book-bold', color: '#a7f' },
+			资源: { icon: 'ph:folder-notch-bold', color: '#9a1' },
 		},
 		defaultCategoryIcon: 'ph:folder-bold',
 		/** 文章版式，首个为默认版式 */
@@ -51,7 +53,7 @@ const blogConfig = {
 			// title: '标题',
 		},
 		/** 使用 pnpm new 新建文章时自动生成自定义链接（permalink/abbrlink） */
-		useRandomPremalink: false,
+		useRandomPremalink: true,
 		/** 隐藏基于文件路由（不是自定义链接）的 URL /post 路径前缀 */
 		hidePostPrefix: true,
 		/** 禁止搜索引擎收录的路径 */
@@ -69,24 +71,22 @@ const blogConfig = {
 	/** 向 <head> 中添加脚本 */
 	scripts: [
 		// 自己部署的 Umami 统计服务
-		{ 'src': 'https://zhi.example.site/zhi.js', 'data-website-id': 'a1997c81-a42b-46f6-8d1d-8fbd67a8ef41', 'defer': true },
-		// 自己网站的 Cloudflare Insights 统计服务
-		{ 'src': 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "97a4fe32ed8240ac8284e9bffaf03962"}', 'defer': true },
+		{ 'src': 'https://cors.vacu.top/https://cloud.umami.is/script.js', 'data-website-id': '1a3c2e77-3310-4e7a-af8f-e93c85cc9ada', 'defer': true },
 		// Twikoo 评论系统
 		{ src: 'https://lib.baomitu.com/twikoo/1.6.44/twikoo.min.js', defer: true },
 	],
 
 	/** 自己部署的 Twikoo 服务 */
 	twikoo: {
-		envId: 'https://twikoo.example.site/',
-		preload: 'https://twikoo.example.site/',
+		envId: 'https://vacuoletwikoo.netlify.app/.netlify/functions/twikoo',
+		preload: 'https://vacuoletwikoo.netlify.app/.netlify/functions/twikoo',
 	},
 }
 
 /** 用于生成 OPML 和友链页面配置 */
 export const myFeed: FeedEntry = {
 	author: blogConfig.author.name,
-	sitenick: '摸鱼处',
+	sitenick: '部落格',
 	title: blogConfig.title,
 	desc: blogConfig.subtitle || blogConfig.description,
 	link: blogConfig.url,
