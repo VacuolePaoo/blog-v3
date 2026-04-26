c68f246919e9d1275abc97248120af418872f5e9
vacuolepaoo <zephyrionvossington@proton.me>
2026-04-26 23:00:37 +0800

    new post

--- app/feeds.ts
+++ app/feeds.ts
@@ -13,8 +13,8 @@ export default [
 				desc: '纸鹿至麓不知路，支炉制露不止漉',
 				link: 'https://blog.zhilu.site/',
 				avatar: 'https://www.zhilu.site/api/avatar.png',
-				date: '2026-02-07',
 				icon: getFavicon('blog.zhilu.site'),
+				date: '2026-02-07',
 				feed: 'https://blog.zhilu.site/atom.xml',
 				comment: '本博客主题作者，专注用户体验，细节狂魔',
 				archs: ['Nuxt', 'Vercel'],
@@ -34,36 +34,34 @@ export default [
 				author: '云森🍥🍥🍥',
 				title: 'Yunsen的小窝',
 				desc: '行之，皆为乐意',
-				date: '2026-04-10',
-				avatar: 'https://img.alicdn.com/bao/uploaded/i4/O1CN01TWOpM42DSyY5nC0hM_!!0-mtopupload.jpg',
 				link: 'https://blog.imysen.com/',
+				avatar: 'https://img.alicdn.com/bao/uploaded/i4/O1CN01TWOpM42DSyY5nC0hM_!!0-mtopupload.jpg',
 				icon: getFavicon('blog.imysen.com'),
+				date: '2026-04-10',
 				feed: 'https://blog.imysen.com/feed/',
 			},
 		],
 	},
-	// #region 网上邻居
 	{
 		name: '网上邻居',
 		desc: '哔——啵——电波通讯中，欢迎常来串门。',
-		// @keep-sorted { "keys": ["date"] }
 		entries: [
 			{
 				author: 'Nahida',
-				link: 'https://nahida.im/',
-				avatar: 'https://nahida.im/profile.png',
-				date: '2026-02-07',
-				icon: 'https://nahida.im/favicon.ico',
 				title: 'Nahida的个人网站',
 				desc: '为世界上一切美好而战！',
-				comment: '社区内活跃成员，曾经搞崩过群组',
+				link: 'https://nahida.im/',
+				avatar: 'https://nahida.im/profile.png',
+				icon: 'https://nahida.im/favicon.ico',
+				date: '2026-02-07',
 				feed: 'https://nahida.im/rss.xml',
+				comment: '社区内活跃成员，曾经搞崩过群组',
+				archs: ['Astro'],
 			},
-
 			{
 				author: '张洪Heo',
-				link: 'https://blog.zhheo.com/',
 				desc: '分享设计与科技生活',
+				link: 'https://blog.zhheo.com/',
 				avatar: 'https://img.zhheo.com/i/67d8fa75943e4.webp',
 				icon: getFavicon('blog.zhheo.com'),
 				date: '2026-02-07',
@@ -72,5 +70,4 @@ export default [
 			},
 		],
 	},
-// #endregion
 ] satisfies FeedGroup[]
